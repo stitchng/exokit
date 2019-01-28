47390d1cc62f6b48ea0e63d3ee5fbd548b7a4ffd
Avaer Kazmer <a@modules.io>
2019-01-27 22:32:48 -0500

    Wait for window syncs in index.js

--- src/index.js
+++ src/index.js
@@ -352,74 +352,6 @@ class XRState {
 GlobalContext.xrState = new XRState();
 
 const _startRenderLoop = () => {
-  const _blit = () => {
-    for (let i = 0; i < contexts.length; i++) {
-      const context = contexts[i];
-      
-      // XXX wait for child gl sync object
-
-      const isDirty = (!!context.isDirty && context.isDirty()) || mlPresentState.mlGlContext === context; // XXX do we need to track dirty?
-      if (isDirty) {
-        const windowHandle = context.getWindowHandle();
-
-        const {nativeWindow} = nativeBindings;
-        nativeWindow.setCurrentWindowContext(windowHandle);
-        if (isMac) {
-          context.flush();
-        }
-
-        const isVisible = nativeWindow.isVisible(windowHandle) || vrPresentState.glContext === context || mlPresentState.mlGlContext === context; // XXX track visible/width/height via child event emits
-        if (isVisible) {
-          // console.log('blit layers', fakePresentState.layers.length);
-
-          if (vrPresentState.glContext === context && vrPresentState.hasPose) {
-            if (vrPresentState.layers.length > 0) {
-              nativeWindow.composeLayers(context, vrPresentState.fbo, vrPresentState.layers, xrState);
-            } else {
-              nativeWindow.blitFrameBuffer(context, vrPresentState.msFbo, vrPresentState.fbo, vrPresentState.glContext.canvas.width, vrPresentState.glContext.canvas.height, vrPresentState.glContext.canvas.width, vrPresentState.glContext.canvas.height, true, false, false);
-            }
-
-            vrPresentState.compositor.Submit(context, vrPresentState.tex);
-            vrPresentState.hasPose = false;
-
-            nativeWindow.blitFrameBuffer(context, vrPresentState.fbo, 0, vrPresentState.glContext.canvas.width * (args.blit ? 0.5 : 1), vrPresentState.glContext.canvas.height, xrState.renderWidth[0], xrState.renderHeight[0], true, false, false);
-          } else if (mlPresentState.mlGlContext === context && mlPresentState.mlHasPose) {
-            if (mlPresentState.layers.length > 0) { // TODO: composition can be directly to the output texture array
-              nativeWindow.composeLayers(context, mlPresentState.mlFbo, mlPresentState.layers, xrState);
-            } else {
-              nativeWindow.blitFrameBuffer(context, mlPresentState.mlMsFbo, mlPresentState.mlFbo, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height, true, false, false);
-            }
-
-            mlPresentState.mlContext.SubmitFrame(mlPresentState.mlTex, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height);
-            mlPresentState.mlHasPose = false;
-
-            // nativeWindow.blitFrameBuffer(context, mlPresentState.mlFbo, 0, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height, xrState.renderWidth[0], xrState.renderHeight[0], true, false, false);
-          } else if (fakePresentState.layers.length > 0) { // XXX blit only to the intended context
-            nativeWindow.composeLayers(context, 0, fakePresentState.layers, xrState);
-          }
-        }
-
-        if (isMac) {
-          context.bindFramebufferRaw(context.FRAMEBUFFER, null);
-        }
-        nativeWindow.swapBuffers(windowHandle); // XXX swap buffers on the child side
-        if (isMac) {
-          const drawFramebuffer = context.getFramebuffer(context.DRAW_FRAMEBUFFER);
-          if (drawFramebuffer) {
-            context.bindFramebuffer(context.DRAW_FRAMEBUFFER, drawFramebuffer);
-          }
-
-          const readFramebuffer = context.getFramebuffer(context.READ_FRAMEBUFFER);
-          if (readFramebuffer) {
-            context.bindFramebuffer(context.READ_FRAMEBUFFER, readFramebuffer);
-          }
-        }
-
-        context.clearDirty();
-      }
-    }
-  }
-
   const timestamps = {
     frames: 0,
     last: Date.now(),
@@ -831,9 +763,76 @@ const _startRenderLoop = () => {
       timestamps.last = now;
     }
     
-    // composite image
+    // blit composite image
+    {
+      for (let i = 0; i < contexts.length; i++) {
+        const context = contexts[i];
+        const sync = syncs.find(sync => sync.window === context.window && sync.id === context.id);
+        if (sync) {
+          nativeWindow.waitSync(sync.sync);
+        }
 
-    _blit();
+        const isDirty = (!!context.isDirty && context.isDirty()) || mlPresentState.mlGlContext === context; // XXX do we need to track dirty?
+        if (isDirty) {
+          const windowHandle = context.getWindowHandle();
+
+          const {nativeWindow} = nativeBindings;
+          nativeWindow.setCurrentWindowContext(windowHandle);
+          if (isMac) { // XXX move these to window internal
+            context.flush();
+          }
+
+          const isVisible = nativeWindow.isVisible(windowHandle) || vrPresentState.glContext === context || mlPresentState.mlGlContext === context; // XXX track visible/width/height via child event emits
+          if (isVisible) {
+            // console.log('blit layers', fakePresentState.layers.length);
+
+            if (vrPresentState.glContext === context && vrPresentState.hasPose) {
+              if (vrPresentState.layers.length > 0) {
+                nativeWindow.composeLayers(context, vrPresentState.fbo, vrPresentState.layers, xrState);
+              } else {
+                nativeWindow.blitFrameBuffer(context, vrPresentState.msFbo, vrPresentState.fbo, vrPresentState.glContext.canvas.width, vrPresentState.glContext.canvas.height, vrPresentState.glContext.canvas.width, vrPresentState.glContext.canvas.height, true, false, false);
+              }
+
+              vrPresentState.compositor.Submit(context, vrPresentState.tex);
+              vrPresentState.hasPose = false;
+
+              nativeWindow.blitFrameBuffer(context, vrPresentState.fbo, 0, vrPresentState.glContext.canvas.width * (args.blit ? 0.5 : 1), vrPresentState.glContext.canvas.height, xrState.renderWidth[0], xrState.renderHeight[0], true, false, false);
+            } else if (mlPresentState.mlGlContext === context && mlPresentState.mlHasPose) {
+              if (mlPresentState.layers.length > 0) { // TODO: composition can be directly to the output texture array
+                nativeWindow.composeLayers(context, mlPresentState.mlFbo, mlPresentState.layers, xrState);
+              } else {
+                nativeWindow.blitFrameBuffer(context, mlPresentState.mlMsFbo, mlPresentState.mlFbo, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height, true, false, false);
+              }
+
+              mlPresentState.mlContext.SubmitFrame(mlPresentState.mlTex, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height);
+              mlPresentState.mlHasPose = false;
+
+              // nativeWindow.blitFrameBuffer(context, mlPresentState.mlFbo, 0, mlPresentState.mlGlContext.canvas.width, mlPresentState.mlGlContext.canvas.height, xrState.renderWidth[0], xrState.renderHeight[0], true, false, false);
+            } else if (fakePresentState.layers.length > 0) { // XXX blit only to the intended context
+              nativeWindow.composeLayers(context, 0, fakePresentState.layers, xrState);
+            }
+          }
+
+          if (isMac) {
+            context.bindFramebufferRaw(context.FRAMEBUFFER, null);
+          }
+          nativeWindow.swapBuffers(windowHandle); // XXX swap buffers on the child side
+          if (isMac) {
+            const drawFramebuffer = context.getFramebuffer(context.DRAW_FRAMEBUFFER);
+            if (drawFramebuffer) {
+              context.bindFramebuffer(context.DRAW_FRAMEBUFFER, drawFramebuffer);
+            }
+
+            const readFramebuffer = context.getFramebuffer(context.READ_FRAMEBUFFER);
+            if (readFramebuffer) {
+              context.bindFramebuffer(context.READ_FRAMEBUFFER, readFramebuffer);
+            }
+          }
+
+          context.clearDirty();
+        }
+      }
+    }
 
     // lastFrameTime = Date.now()
 
